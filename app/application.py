48ef6cf8183905e325cd4a072577a4d2c36dcae2
Dominik <d.hendrix@einfachreservieren.de>
2015-10-13 09:36:44 +0200

    init-commit

---
+++ app/application.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+import cherrypy
+import json
+# --------------------------------------
+
+
+class Application_cl(object):
+	# --------------------------------------
+	# ----------------------------------
+	def __init__(self):
+		# --------------------------------------
+		# constructor
+		pass
+		# --------------------------------------
+	# Methode "module" definieren
+
+	def module(self):
+		# --------------------------------------
+		# Datei "module.json" im Lesemodus oeffnen
+		json_data = open("data/module.json", "r")
+		data = json.loads(json_data.read())
+		# Inhalt von Datei zurueckgeben
+		return json_data
+
+	module.exposed = True
+	# --------------------------------------
+
+	def	default(self, *arglist, **kwargs):
+		# --------------------------------------
+		msg_s =	"unbekannte Anforderung: " + \
+			str(arglist) + \
+			'' + \
+			str(kwargs)
+
+		raise cherrypy.HTTPError(404, msg_s)
+
+	default.exposed = True
+# EOF
